3968e7d2a4a8b799b6db3b046811c2bd5f19e6e7
illubaby <aironb177@gmail.com>
2023-11-20 23:44:37 +0700

    123

---
+++ UI demo/Temp VNese/StudCfmPr.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded",function(){
+    var Addbutton = document.getElementById("Addbutton");
+    if (Addbutton){
+        Addbutton.addEventListener("click",function(){
+            alert("Đã nhấn mặc định");
+        });
+    }
+});
